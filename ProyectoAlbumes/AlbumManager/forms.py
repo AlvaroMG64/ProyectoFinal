970643e94e83bf233dd4510e9b82bf019cebe273
AlvaroMG64 <ciencias8000@gmail.com>
2026-02-14 19:52:09 +0100

    Actualizaciones

--- ProyectoAlbumes/AlbumManager/forms.py
+++ ProyectoAlbumes/AlbumManager/forms.py
@@ -1,33 +1,41 @@
 from django import forms
 from .models import Artista, Album, Cancion
 
+
 class ArtistaForm(forms.ModelForm):
     class Meta:
         model = Artista
-        fields = ["nombre", "pais", "genero"]
+        fields = "__all__"
         widgets = {
-            'nombre': forms.TextInput(attrs={'class': 'form-control'}),
-            'pais': forms.TextInput(attrs={'class': 'form-control'}),
-            'genero': forms.TextInput(attrs={'class': 'form-control'}),
+            "nombre": forms.TextInput(attrs={"class": "form-control"}),
+            "pais": forms.TextInput(attrs={"class": "form-control"}),
+            "genero": forms.TextInput(attrs={"class": "form-control"}),
         }
+
 
 class AlbumForm(forms.ModelForm):
     class Meta:
         model = Album
-        fields = ["titulo", "artista", "fecha_lanzamiento", "es_explicit"]
+        fields = "__all__"
         widgets = {
-            'titulo': forms.TextInput(attrs={'class': 'form-control'}),
-            'artista': forms.Select(attrs={'class': 'form-select'}),
-            'fecha_lanzamiento': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
-            'es_explicit': forms.RadioSelect(choices=[(True, 'Sí'), (False, 'No')]),
+            "titulo": forms.TextInput(attrs={"class": "form-control"}),
+            "artista": forms.Select(attrs={"class": "form-select"}),
+            "fecha_lanzamiento": forms.DateInput(attrs={
+                "class": "form-control",
+                "type": "date"
+            }),
+            "num_canciones": forms.NumberInput(attrs={"class": "form-control"}),
+            "es_explicit": forms.RadioSelect(choices=[(True, "Sí"), (False, "No")]),
+            "usuario": forms.Select(attrs={"class": "form-select"}),
         }
+
 
 class CancionForm(forms.ModelForm):
     class Meta:
         model = Cancion
-        fields = ["titulo", "album", "duracion"]
+        fields = "__all__"
         widgets = {
-            'titulo': forms.TextInput(attrs={'class': 'form-control'}),
-            'album': forms.Select(attrs={'class': 'form-select'}),
-            'duracion': forms.TimeInput(attrs={'type':'time', 'class': 'form-control'}),
+            "titulo": forms.TextInput(attrs={"class": "form-control"}),
+            "album": forms.Select(attrs={"class": "form-select"}),
+            "duracion": forms.NumberInput(attrs={"class": "form-control"}),
         }
